056f38405943cfaca91989fcedea3839a9690d3a
Heldson L Silva <heldsonluiz@gmail.com>
2023-06-02 13:53:14 -0300

    :sparkles: Páginas e rotas

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,16 +8,17 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    outline: 0cap;
+    outline: 0;
     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
+    --webkit-font-smoothing: antialiased
   }
 
-  body, input-security, text-area, button {
+  body, input, text-area, button {
     font-family:  'Roboto', sans-serif;
     font-size:  1rem;
     font-weight:  400;
